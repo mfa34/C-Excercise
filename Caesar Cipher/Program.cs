c971e92e15fcb42e43a36dfa78afd52335ea9d4e
mfa34 <mfa34@protonmail.com>
2023-09-28 10:15:59 +0300

    Caesar Cipher file added 1.0 versiyonu

---
+++ Caesar Cipher/Program.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      
+            Console.WriteLine("Lütfen mesajınızı yazınız...");
+            string input = Console.ReadLine();
+            string inputLower = input.ToLower();
+      
+            char[] secretMessage = inputLower.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char letter = secretMessage[i];
+                if (char.IsLetter(letter)) // Check if the character is a letter
+                {
+                    int letterPosition = Array.IndexOf(alphabet, letter);
+                    // Burada alfabeyi modunu alarak tekrardan ayni sekilde alfabeye donmesini sagliyoruz.
+                    int newLetterPosition = (letterPosition + 3) % 26;
+                    char letterEncoded = alphabet[newLetterPosition];
+                    encryptedMessage[i] = letterEncoded;
+                }
+                else
+                {
+                    encryptedMessage[i] = letter; // If it's not a letter, keep it as is
+                }
+            }
+            string encodedString = new string(encryptedMessage);
+            Console.WriteLine($"Your encoded message is: {encodedString}");
+        }
+    }
+}
