5e3f008b8351c5a9cd25ffc51c762f7a143fbf0d
Mehmet Fatih Aydın <105373205+mfa34@users.noreply.github.com>
2023-09-28 10:45:52 +0300

    Update Comment

--- Caesar Cipher/Program.cs
+++ Caesar Cipher/Program.cs
@@ -8,7 +8,7 @@ namespace CaesarCipher
         {
             char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
       
-            Console.WriteLine("Lütfen mesajınızı yazınız...");
+            Console.WriteLine("Enter your message...");
             string input = Console.ReadLine();
             string inputLower = input.ToLower();
       
